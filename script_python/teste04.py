f852a383acda58b9735f66db083225782ac3d156
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-06-02 14:05:10 -0300

    aula 4 add

--- script_python/teste04.py
+++ script_python/teste04.py
@@ -2,4 +2,4 @@ print('====== DESAFIO 03 ======')
 n1 = int(input('Primeiro número '))
 n2 = int(input('Segundo número '))
 soma = n1 + n2
-print('A soma de {} com {} é {}'.format(n1, n2, soma) )
+print('A soma entre {} e {} é {}'.format(n1, n2, soma) )
