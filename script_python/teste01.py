6e3f6cacc54e61a7768425069fea55e82e9a29c6
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-06-02 10:05:57 -0300

    Adição da pasta de script

---
+++ script_python/teste01.py
@@ -0,0 +1,4 @@
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+peso = input("Qual o seu peso? ")
+print("Seu nome é", nome, ", sua idade é ", idade, " e seu peso é ", peso)
---
+++ script_python/teste04.py
@@ -0,0 +1,5 @@
+print('====== DESAFIO 03 ======')
+n1 = int(input('Primeiro número '))
+n2 = int(input('Segundo número '))
+soma = n1 + n2
+print('A soma de {} com {} é {}'.format(n1, n2, soma) )
