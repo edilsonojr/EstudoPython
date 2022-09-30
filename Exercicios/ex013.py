c12b142d4654e88287a5380cc9a7b89aedffed77
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-09-30 15:25:51 -0300

    Ajustes no exercicio 13

--- Exercicios/ex013.py
+++ Exercicios/ex013.py
@@ -1,5 +1,5 @@
 sal = float(input('Digite seu salário: '))
 aj = sal * 0.15
 
-print('##############################')
-print('Seu salário é R$:{:.3f}, o seu novo salário com ajuste de 15% é R$:{:.3f}'.format(sal, sal + aj).replace('.', ','))
+print('#' * 20)
+print('Seu salário atual é R$:{:.2f} e o seu novo salário com ajuste de 15% será de R$:{:.2f}'.format(sal, sal + aj).replace('.', ','))
