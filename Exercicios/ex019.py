e393ae71abdca847c633521766f7a53e6152fabf
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-10-03 17:12:13 -0300

    Adicionado o exercício 19

---
+++ Exercicios/ex019.py
@@ -0,0 +1,11 @@
+"""Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido"""
+from random import choice
+
+a1 = str(input("Digite o nome do primeiro aluno: "))
+a2 = str(input("Digite o nome do segundo aluno: "))
+a3 = str(input("Digite o nome do terceiroz aluno: "))
+a4 = str(input("Digite o nome do quarto aluno: "))
+lista = [a1, a2, a3, a4]
+escolhido = choice(lista)
+print("O aluno escolhido foi {}".format(escolhido))
