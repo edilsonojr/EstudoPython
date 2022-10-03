4e30170a3caed61417f1b793bb275b13f9ca7f62
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-10-03 09:38:26 -0300

    adicionado a aula 08

---
+++ script_python/08-UtilizandoModulos.py
@@ -0,0 +1,19 @@
+"""UTILIZANDO A BIBLIOTECA COMPLETA DO MODULO
+import math
+
+num = int(input("Digite um número inteiro: "))
+raiz = math.sqrt(num)
+print("A raiz quadrada de {:.0f} é {:.0f}".format(num, raiz))"""
+
+
+#Importado apenas a funcionalidade sqrt do modulo math
+import emoji
+
+print(emoji.emojize('Olá, Mundo 🌎'))
+
+"""
+from math import sqrt
+
+num = int(input("Digite um número inteiro: "))
+raiz = sqrt(num)
+print("A raiz quadrada de {:.0f} é {:.0f}".format(num, raiz))"""
