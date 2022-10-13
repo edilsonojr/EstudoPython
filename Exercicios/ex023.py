e1bb58de6043d72907ffab934005398e816f4ece
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-10-13 14:53:32 -0300

    Corrigido ex023.py

--- Exercicios/ex023.py
+++ Exercicios/ex023.py
@@ -5,8 +5,12 @@
 #Centena: 8
 #Milhar: 1
 
-numero = str(input("Digite um número entre 0 e 9999: "))
-print("Unidade->", numero[3])
-print("Dezena-->", numero[2])
-print("Centena->", numero[1])
-print("Milhar-->", numero[0])
+numero = int(input("Digite um número entre 0 e 9999: "))
+uni = numero // 1 % 10
+dez = numero // 10 % 10
+cen = numero // 100 % 10
+mil = numero // 1000 % 10
+print("Unidade--> {}".format(uni))
+print("Dezena---> {}".format(dez))
+print("Centena--> {}".format(cen))
+print("Milhar---> {}".format(mil))
