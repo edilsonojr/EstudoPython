1241e69f87063cbda46ba0e9ed5707b378b0ed08
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-01 15:51:38 -0300

    Ajuste na descrição do print

--- Exercicios/ex007.py
+++ Exercicios/ex007.py
@@ -1,4 +1,4 @@
 n1 = float(input("Digite a primeira nota: "))
 n2 = float(input("Digite a segunda nota: "))
 m = (n1 + n2) / 2
-print("A sua média entre {:.1f} e {:.1f} é igual a {:.1f}".format(n1, n2, m))
+print("A média entre {:.1f} e {:.1f} é igual a {:.1f}".format(n1, n2, m))
