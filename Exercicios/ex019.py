afa77c01267dd5e25095743bb8e7338bdfc68652
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-07 15:25:49 -0300

    Ajustes simples

--- Exercicios/ex019.py
+++ Exercicios/ex019.py
@@ -4,8 +4,8 @@ from random import choice
 
 a1 = str(input("Digite o nome do primeiro aluno: "))
 a2 = str(input("Digite o nome do segundo aluno: "))
-a3 = str(input("Digite o nome do terceiroz aluno: "))
+a3 = str(input("Digite o nome do terceiro aluno: "))
 a4 = str(input("Digite o nome do quarto aluno: "))
-lista = [a1, a2, a3, a4]
+lista = [a1, a2, a3, a4] #vetor 
 escolhido = choice(lista)
 print("O aluno escolhido foi {}".format(escolhido))
--- Exercicios/ex022.py
+++ Exercicios/ex022.py
@@ -4,8 +4,8 @@
 #Quantas letras ao todo(Sem considerar espaços)
 #Quantas letras tem o primeiro nome.
 
-nome = str(input("Digite seu nome completo: ")).strip()
+nome = str(input("Digite seu nome completo: ")).strip()#strip remove espaços no começo e no final
 print("Todas letras em maiúsculas: ", nome.upper())
 print("Todas letras em minúsculas: ", nome.lower())
-print("Total de letras sem espaços antes e depois: {}".format(len(nome) - nome.count(" ")))
-print("Número de letras no primeiro nome: ", len(nome.split()[0]))
+print("Total de letras sem espaços antes e depois: {}".format(len(nome) - nome.count(" ")))#total caracter - total espaço
+print("Número de letras no primeiro nome: ", len(nome.split()[0]))#separa o nome e aponta para o nome na posição "0"
