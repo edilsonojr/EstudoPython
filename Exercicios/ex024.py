445dcbc0cd6f4875fbf09e7c2080f82d2c3a77f3
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-09 14:37:26 -0300

    Ajustes simples

--- Exercicios/ex024.py
+++ Exercicios/ex024.py
@@ -2,7 +2,7 @@
 cidade = str(input("Digite o nome da cidade: "))
 cidade = cidade.upper().split()[0]
 #print(cidade)
-verificador = 'SANTO' in cidade
+verificador = 'SANTO' in cidade #variável verificador receberá um valor boleano
 if verificador == True:
     print("A cidade começa com SANTO")
 else:
