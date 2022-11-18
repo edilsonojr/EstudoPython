fc884ce48800339fcb634761400ffb63bd1a781b
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-18 08:38:36 -0300

    Correcao ex033.py

--- Exercicios/ex033.py
+++ Exercicios/ex033.py
@@ -1,4 +1,5 @@
 #FAÇA UM PROGRAMA QUE LEIA TRÊS NÚMEROS E MOSTRE QUAL É O MAIOR E QUAL É O MENOR
+'''
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 n3 = float(input("Digite o terceiro número: "))
@@ -18,3 +19,22 @@ elif n2 < n1 and n2 < n3:
     print("O segundo valor digitado é o menor -> {:.2f}".format(n2))
 else:
     print("O terceiro valor digitado é o menor -> {:.2f}".format(n3))
+'''
+### OUTRA FORMA DE RESOLVER ###
+a = int(input("Primeiro valor: "))
+b = int(input("Segundo valor: "))
+c = int(input("Terceiro valor: "))
+# Verificando quem é menor
+menor = a
+if b < a and b < c:
+    menor = b
+if c < a and c < b:
+    menor = c
+# Verificando quem é maior
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+print("O menor valor digitado foi {}".format(menor))
+print("O maior valor digitado foi {}".format(maior))
