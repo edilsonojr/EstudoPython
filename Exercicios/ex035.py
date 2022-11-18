866c75df14b7d3a90d456cdee9a7cba3e79f688f
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-18 17:23:29 -0300

    Correcao ex035.py

--- Exercicios/ex035.py
+++ Exercicios/ex035.py
@@ -1,4 +1,5 @@
 #DESENVOLVA UM PROGRAMA QUE LEIA O COMPRIMENTO DE TRÊS RETAS E DIGA AO USUÁRIO SE ELAS PODEM OU NÃO FORMAR UM TRIÂGULO
+'''
 r1 = float(input("Digite o comprimento da primeira reta: "))
 r2 = float(input("digite o comprimento da segunda reta: "))
 r3 = float(input("Digite o comprimento da terceira reta: "))
@@ -6,3 +7,15 @@ if r1+r2 > r3 and r1+r3 > r2 and r2+r3 > r1:
     print("As retas formam um triângulo!")
 else:
     print("As retas não formam um triângulo!")
+'''
+### OUTRA FORMA DE RESOLVER ###
+print("-=-"*20)
+print("Analisador de triângulos")
+print("-=-"*20)
+r1 = float(input("Primeiro segmento: "))
+r2 = float(input("Segundo segmento: "))
+r3 = float(input("Terceiro segmento: "))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print("Os segmentos acima PODEM FORMAR UM TRIÂNGULO!")
+else:
+    print("Os segmentos acima NÃO PODEM FORMAR UM TRIÂNGULO!")
