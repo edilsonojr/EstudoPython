37d82cc4a2df39ead22a8c52456b17007791a1ea
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-21 17:33:43 -0300

    add color exercicio 05

--- Mundo_01/Exercicios/ex005.py
+++ Mundo_01/Exercicios/ex005.py
@@ -1,2 +1,5 @@
-n = int(input('Digite um número inteiro: '))
-print('O número digitado é {}, seu antecessor é {} e seu sucessor é {}'.format(n, n-1, n+1))
+cores = {"preto":"\033[1;35m",
+        "vermelho":"\033[1;31m"}
+
+n = int(input(cores["preto"] + 'Digite um número inteiro: '))
+print(cores["vermelho"] + 'O número digitado é {}, seu antecessor é {} e seu sucessor é {}'.format(n, n-1, n+1))
