0a1cf3ea7ccdc489d6d62164600c4722026570f5
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-21 16:49:08 -0300

    add color exercicio 03

--- Mundo_01/Exercicios/ex003.py
+++ Mundo_01/Exercicios/ex003.py
@@ -5,10 +5,11 @@
 # n2 = input('Digite mais um número: ')
 # s = n1 + n2
 # print('A soma vale: ', s)
-
+cores = {"verde":"\033[32m",
+        "verdenegrito":"\033[1;32m"}
 
 #Aqui é realizado uma conversão do tipo para inteiro
-n1 = int(input('Digite um número: '))
+n1 = int(input(cores["verde"] + 'Digite um número: '))
 n2 = int(input('Digite mais um número: '))
 s = n1 + n2
-print('A soma entre {} e {} vale {}'.format(n1, n2, s))
+print('A soma entre {} e {} vale {}{}'.format(n1, n2,cores["verdenegrito"] ,s))
