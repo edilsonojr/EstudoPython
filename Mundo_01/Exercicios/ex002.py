d166e29b3ebd059cd86599f79a1d287fc646d664
Edilson de Oliveira Júnior <edilson.oliveira@mv.com.br>
2022-11-21 16:33:56 -0300

    add color exercicio 02

--- Mundo_01/Exercicios/ex002.py
+++ Mundo_01/Exercicios/ex002.py
@@ -1,2 +1,6 @@
-nome = input('Digite seu nome: ')
-print('É um prazer te conhecer, {}!'.format(nome))
+cores = {"azul":"\033[34m",
+        "amarelo":"\033[33m",
+        "amarelonegrito":"\033[1;33m"}
+
+nome = input(cores["azul"] + 'Digite seu nome: '+ "\033[m")
+print(cores["amarelo"] + 'É um prazer te conhecer, {}{}!'.format(cores["amarelonegrito"],nome))
